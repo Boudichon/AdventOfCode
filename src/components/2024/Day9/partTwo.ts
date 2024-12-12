2b7bae368d56b01616ed5a61b60402c082d88839
Dominic Cyr <cyrd97@gmail.com>
2024-12-12 16:56:59 -0500

    Day 9 p 2

--- src/components/2024/Day9/partTwo.ts
+++ src/components/2024/Day9/partTwo.ts
@@ -6,40 +6,52 @@ export default {
         const input = await GetInput(2024, 9);
         let total = 0;
 
-        const inputArray = [...input].filter((_, i) => i % 2 == 0).map(x => Number.parseInt(x));
+        const spaces: Block[] = [...input].map((x, i) => {
+            return {
+                isUsed: i % 2 == 0,
+                value: Math.floor(i / 2),
+                size: Number.parseInt(x)
+            };
+        });
 
-        const spaceUsed = inputArray.reduce((a, b) => a + b, 0);
+        for (let i = spaces.length - 1; i >= 0; --i) {
+            const block = spaces[i];
+
+            if (block.isUsed) {
+                const freeIndex = spaces.findIndex(x => !x.isUsed && x.size >= block.size);
+
+                if (freeIndex >= 0 && freeIndex < i) {
+                    spaces[freeIndex].size -= block.size;
+                    spaces.splice(i, 1, {
+                        isUsed: false,
+                        size: block.size,
+                        value: -1 // It's unused so it doesn't matter
+                    });
+                    spaces.splice(freeIndex, 0, block);
+                    i++;
+                }
+            }
+        }
 
         let index = 0;
-        const blocks: number[] = [];
-
-        for (let i = 0; i < input.length; ++i) {
-            const number = Number.parseInt(input[i]);
-
-            for (let j = 0; j < number; ++j) {
-                if (i % 2 == 0) {                    
-                    blocks.push(i / 2);
-                    total += index * (i / 2);
-                } else {
-                    while (inputArray[inputArray.length - 1] > number) {
-                        inputArray.pop();
-                    }
-                    blocks.push(inputArray.length - 1);
-                    total += index * (inputArray.length - 1);
-
-                    inputArray[inputArray.length - 1] = inputArray[inputArray.length - 1] - 1;
+        spaces.forEach(x => {
+            while (x.size > 0) {
+                if (x.isUsed) {
+                    total += index * x.value;
                 }
-
+                --x.size;
                 index++;
-                // if (index + 1 >= spaceUsed) break;
             }
-            // console.log(blocks.join(''));
-            // console.log("0099211177744333555566668888")
-            // if (index + 1 >= spaceUsed) break;
-        }
+        });
 
         const solution = total;
 
         return { solution };
     },
+}
+
+interface Block {
+    size: number,
+    value: number,
+    isUsed: boolean;
 }
